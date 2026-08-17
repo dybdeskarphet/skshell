86cc522d8d489dade91f3be61fd9521affe05a26
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-08-18 02:23:08 +0300

    refactor(templates): move Module to core and add click offset calculation

--- src/widget/templates/core/Module.tsx
+++ src/widget/templates/core/Module.tsx
@@ -1,12 +1,13 @@
 import Gtk from "gi://Gtk?version=4.0";
 import { Accessor, createState } from "gnim";
 import { IslandContext } from "../../store/islandContext";
+import Gdk from "gi://Gdk?version=4.0";
 
 interface ModuleProps {
   icon?: string | Accessor<string>;
   text?: string | Accessor<string>;
   showOnHover?: boolean;
-  onClicked?: () => void;
+  onClicked?: (posX: number) => void;
   cssClasses?: string[];
 }
 
@@ -18,7 +19,8 @@ export const Module = ({
   cssClasses,
 }: ModuleProps) => {
   const [hovered, setHovered] = createState(false);
-  const { keepOpen, scheduleClose } = IslandContext.use();
+  const { keepOpen, scheduleClose, gdkmonitor } = IslandContext.use();
+  const monitorWidth = gdkmonitor.get_geometry().width;
 
   const content = (
     <box
@@ -39,32 +41,11 @@ export const Module = ({
     </box>
   );
 
-  if (onClicked) {
-    return (
-      <button
-        $={(self) => {
-          const motion = new Gtk.EventControllerMotion();
-          motion.connect("enter", () => {
-            keepOpen();
-            if (showOnHover) setHovered(true);
-          });
-          motion.connect("leave", () => {
-            scheduleClose();
-            if (showOnHover) setHovered(false);
-          });
-          self.add_controller(motion);
-        }}
-        onClicked={onClicked}
-        css_classes={["module", "clickable", ...(cssClasses ?? [])]}
-      >
-        {content}
-      </button>
-    );
-  }
-
   return (
     <box
+      cursor={onClicked ? Gdk.Cursor.new_from_name("pointer", null) : null}
       $={(self) => {
+        // showOnHover logic
         const motion = new Gtk.EventControllerMotion();
         motion.connect("enter", () => {
           keepOpen();
@@ -75,8 +56,34 @@ export const Module = ({
           if (showOnHover) setHovered(false);
         });
         self.add_controller(motion);
+
+        // onClick logic
+        if (onClicked) {
+          const click = new Gtk.GestureClick();
+          click.connect("pressed", () => {
+            const islandBox = self.get_parent();
+            let calculatedOffset = 0;
+            if (islandBox) {
+              const [, buttonBounds] = self.compute_bounds(islandBox);
+              const [, islandBounds] = islandBox.compute_bounds(islandBox);
+
+              const posX = buttonBounds.get_x();
+              const islandWidth = islandBounds.get_width();
+
+              calculatedOffset = Math.round(
+                monitorWidth / 2 + posX - islandWidth / 2,
+              );
+            }
+            onClicked(calculatedOffset);
+          });
+          self.add_controller(click);
+        }
       }}
-      css_classes={["module", ...(cssClasses ?? [])]}
+      css_classes={[
+        "module",
+        ...(onClicked ? ["clickable-module"] : []),
+        ...(cssClasses ?? []),
+      ]}
     >
       {content}
     </box>
