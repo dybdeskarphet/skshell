e2c4fd6bbff8b5c6c81cceaa314066a03eaa5eb3
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-08-19 02:56:42 +0300

    feat(services): implement a very basic clock service

---
+++ src/services/shared/clockService.ts
@@ -0,0 +1,19 @@
+import GLib from "gi://GLib?version=2.0";
+import { createPoll } from "../../utils/pausable";
+
+export class ClockService {
+  private static instance: ClockService;
+
+  public now = createPoll(GLib.DateTime.new_now_local(), 1000, () => {
+    return GLib.DateTime.new_now_local();
+  });
+
+  public time = this.now.as((t) => t.format("%H:%M") ?? "");
+  public timeWithSeconds = this.now.as((t) => t.format("%H:%M:%S") ?? "");
+
+  public static get_default() {
+    return (this.instance ??= new ClockService());
+  }
+}
+
+export const clockService = ClockService.get_default();
