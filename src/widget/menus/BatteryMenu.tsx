9c6e55f1739000657bb08f50a7fc9aa06fcd5c43
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-08-18 06:25:01 +0300

    feat(menus): finalize with a proper battery menu

--- src/widget/menus/BatteryMenu.tsx
+++ src/widget/menus/BatteryMenu.tsx
@@ -2,9 +2,22 @@ import AstalBattery from "gi://AstalBattery?version=0.1";
 import { IslandContext } from "../../store/islandContext";
 import { Menu } from "../templates/core/Menu";
 import { createBinding } from "gnim";
+import { MenuBullet } from "../templates/menus/MenuBullet";
+import { batteryService } from "../../services/shared/batteryService";
 
 export const BatteryMenu = () => {
-  
-
-  return <Menu name="battery" title="Battery" icon="󰁹"></Menu>;
+  return (
+    <Menu name="battery" title="Battery" icon="󰁹">
+      <MenuBullet
+        icon={"󱐋"}
+        label={"Voltage"}
+        value={batteryService.voltageText}
+      />
+      <MenuBullet
+        icon={"󱈏"}
+        label={"Actual capacity"}
+        value={batteryService.capacityText}
+      />
+    </Menu>
+  );
 };
